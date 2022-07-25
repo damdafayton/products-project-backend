93f3163f43b1f500fecd46fbe63f04d33e0a77e9
backtofayton <yucelolcay@gmail.com>
2022-07-25 23:19:08 +0300

    Update db

--- config/configuration.php
+++ config/configuration.php
@@ -1,5 +1,10 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DB_DATABASE_NAME", "test_scandiweb_products");
+// define("DB_HOST", "localhost");
+// define("DB_USERNAME", "root");
+// define("DB_PASSWORD", "");
+// define("DB_DATABASE_NAME", "test_scandiweb_products");
+
+define("DB_HOST", "us-cdbr-east-06.cleardb.net");
+define("DB_USERNAME", "b5c65dcef23a4c");
+define("DB_PASSWORD", "55e8369f");
+define("DB_DATABASE_NAME", "heroku_37d1f22892a0b4d");
