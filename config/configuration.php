d2ee6e1595f8761fd9dcd16f0e1299d9ca809fcb
backtofayton <yucelolcay@gmail.com>
2022-07-26 00:18:17 +0300

    Update db

--- config/configuration.php
+++ config/configuration.php
@@ -1,12 +1,12 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
-define("DB_DATABASE_NAME", "test_scandiweb_products");
+// define("DB_HOST", "localhost");
+// define("DB_USERNAME", "root");
+// define("DB_PASSWORD", "");
+// define("DB_DATABASE_NAME", "test_scandiweb_products");
 
-// define("DB_HOST", "us-cdbr-east-06.cleardb.net");
-// define("DB_USERNAME", "b5c65dcef23a4c");
-// define("DB_PASSWORD", "55e8369f");
-// define("DB_DATABASE_NAME", "heroku_37d1f22892a0b4d");
+define("DB_HOST", "us-cdbr-east-06.cleardb.net");
+define("DB_USERNAME", "b5c65dcef23a4c");
+define("DB_PASSWORD", "55e8369f");
+define("DB_DATABASE_NAME", "heroku_37d1f22892a0b4d");
 
-putenv('CLEARDB_DATABASE_URL=mysql://b5c65dcef23a4c:55e8369f@us-cdbr-east-06.cleardb.net/heroku_37d1f22892a0b4d?reconnect=true');
+// putenv('CLEARDB_DATABASE_URL=mysql://b5c65dcef23a4c:55e8369f@us-cdbr-east-06.cleardb.net/heroku_37d1f22892a0b4d?reconnect=true');
