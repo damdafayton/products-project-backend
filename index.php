67b023a66273fc55344b63c0c1384971dedadcd8
backtofayton <yucelolcay@gmail.com>
2022-07-26 10:59:50 +0300

    HEROKU

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-echo __DIR__;
+// error_reporting(E_ERROR | E_WARNING | E_PARSE);
+// echo __DIR__;
+echo 'HELLO';
 // require_once dirname(__FILE__) . "/config/bootstrap.php";
 
 // $baseController = new BaseController();
