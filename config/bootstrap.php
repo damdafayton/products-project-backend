661fb9fc4c320ed200560aeee56282fc217bfd34
backtofayton <yucelolcay@gmail.com>
2022-07-26 11:13:22 +0300

    HEROKU

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -3,8 +3,10 @@ define("PROJECT_ROOT_PATH", dirname(dirname(__FILE__)));
 
 echo 'HELLO2';
 
+echo PROJECT_ROOT_PATH;
+
 // // include main configuration file
-// require_once PROJECT_ROOT_PATH . "/config/configuration.php";
+require_once PROJECT_ROOT_PATH . "/config/configuration.php";
 
 // // helpers
 // require_once PROJECT_ROOT_PATH . "/helpers/utils.php";
--- config/configuration.php
+++ config/configuration.php
@@ -3,6 +3,7 @@
 // define("DB_USERNAME", "root");
 // define("DB_PASSWORD", "");
 // define("DB_DATABASE_NAME", "test_scandiweb_products");
+echo 'HELLO3';
 
 define("DB_HOST", "us-cdbr-east-06.cleardb.net");
 define("DB_USERNAME", "b5c65dcef23a4c");
@@ -10,3 +11,5 @@ define("DB_PASSWORD", "55e8369f");
 define("DB_DATABASE_NAME", "heroku_37d1f22892a0b4d");
 
 // putenv('CLEARDB_DATABASE_URL=mysql://b5c65dcef23a4c:55e8369f@us-cdbr-east-06.cleardb.net/heroku_37d1f22892a0b4d?reconnect=true');
+
+echo 'HELLO4';
