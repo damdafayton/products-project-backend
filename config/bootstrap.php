c94c97aa7eba9d3b4287e92687266bf6fff8fe19
backtofayton <yucelolcay@gmail.com>
2022-07-26 11:09:54 +0300

    HEROKU

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,23 +1,25 @@
 <?php
 define("PROJECT_ROOT_PATH", dirname(dirname(__FILE__)));
 
-// include main configuration file
-require_once PROJECT_ROOT_PATH . "/config/configuration.php";
+echo 'HELLO2';
 
-// helpers
-require_once PROJECT_ROOT_PATH . "/helpers/utils.php";
+// // include main configuration file
+// require_once PROJECT_ROOT_PATH . "/config/configuration.php";
 
-// include the model files
-require_once PROJECT_ROOT_PATH . '/model/database.php';
-require_once PROJECT_ROOT_PATH . '/model/traits/weight.php';
-require_once PROJECT_ROOT_PATH . '/model/traits/size.php';
-require_once PROJECT_ROOT_PATH . '/model/traits/dimensions.php';
-require_once PROJECT_ROOT_PATH . '/model/traits/childMethods.php';
-require_once PROJECT_ROOT_PATH . "/model/product.php";
-require_once PROJECT_ROOT_PATH . "/model/book.php";
-require_once PROJECT_ROOT_PATH . "/model/dvd.php";
-require_once PROJECT_ROOT_PATH . "/model/furniture.php";
+// // helpers
+// require_once PROJECT_ROOT_PATH . "/helpers/utils.php";
 
-// include the base controller file
-require_once PROJECT_ROOT_PATH . "/controllers/baseController.php";
-require_once PROJECT_ROOT_PATH . "/controllers/productController.php";
+// // include the model files
+// require_once PROJECT_ROOT_PATH . '/model/database.php';
+// require_once PROJECT_ROOT_PATH . '/model/traits/weight.php';
+// require_once PROJECT_ROOT_PATH . '/model/traits/size.php';
+// require_once PROJECT_ROOT_PATH . '/model/traits/dimensions.php';
+// require_once PROJECT_ROOT_PATH . '/model/traits/childMethods.php';
+// require_once PROJECT_ROOT_PATH . "/model/product.php";
+// require_once PROJECT_ROOT_PATH . "/model/book.php";
+// require_once PROJECT_ROOT_PATH . "/model/dvd.php";
+// require_once PROJECT_ROOT_PATH . "/model/furniture.php";
+
+// // include the base controller file
+// require_once PROJECT_ROOT_PATH . "/controllers/baseController.php";
+// require_once PROJECT_ROOT_PATH . "/controllers/productController.php";
