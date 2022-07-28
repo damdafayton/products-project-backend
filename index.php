0ccf2edcd2af675013d9f7f88191d7d8582612db
backtofayton <yucelolcay@gmail.com>
2022-07-28 20:19:22 +0300

    Update router

--- index.php
+++ index.php
@@ -4,9 +4,7 @@ error_reporting(E_ERROR | E_WARNING | E_PARSE);
 require_once dirname(__FILE__) . "/src/config/bootstrap.php";
 
 $app = new controller\BaseController();
-$request = $app->request;
-$response = $app->response;
 
-$method =  strtolower($request->getMethod());
+$method =  strtolower($app->request->getMethod());
 
 $app->$method();
